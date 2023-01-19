7edd60897dc976e02dc04dc91f54c6c6cab0ff48
RyanFloresTT <rryanflorres@gmail.com>
2023-01-18 20:07:22 -0800

    Fixed #16. Fixed armor calculation.

--- Assets/Scripts/UI/MenuButtons.cs
+++ Assets/Scripts/UI/MenuButtons.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class PlayButton : MonoBehaviour
+public class MenuButtons : MonoBehaviour
 {
-    public void OnClick()
+    public void OnClickPlay()
     {
         // Load the scene for the first level
         SceneManager.LoadScene("level1");
+    }
+    public void OnClickExit()
+    {
+        // Load the scene for the first level
+        Application.Quit();
     }
 }
