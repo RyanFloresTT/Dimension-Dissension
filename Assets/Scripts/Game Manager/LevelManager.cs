08f293a781dcc66c63aaed009864ea1253581ce7
Ryan Flores <53247675+RyanFloresTT@users.noreply.github.com>
2023-01-22 12:17:17 -0800

    Create a game over scene (#34)
    Close #6 . Created a Game Over scene, and scripts to track player level progression that is called on load of the Game Over scene and can be used for the future sprite loading.

---
+++ Assets/Scripts/Game Manager/LevelManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    // Singleton Setup
+    public static LevelManager instance { get; private set; }
+    void OnEnable() { instance = this; }
+    void OnDisable() { instance = null; }
+    
+    private int _level = 1;
+
+    public int GetLevel()
+    {
+        return _level;
+    }
+
+    public void NextLevel()
+    {
+        _level += 1;
+    }
+    
+}
